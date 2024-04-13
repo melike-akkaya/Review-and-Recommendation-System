3768b7d88e13c2fdec5e7e664b042f2a36586a1e
Berkay Karakaya <b2220356167@cs.hacettepe.edu.tr>
2024-04-13 17:56:06 +0300

    confirmation pop up

--- frontend/sombrero/src/components/merchant/ProductList.js
+++ frontend/sombrero/src/components/merchant/ProductList.js
@@ -6,11 +6,32 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import EditIcon from "@mui/icons-material/Edit";
 import { deleteLabel, deleteProduct } from "../../services/ProductService";
+import ConfirmationDialog from "../merchant/ConformationDialog";
+import { useState } from "react";
+
+//Declare a default image
+const defaultImageBase64 = "iVBORw0KGgoAAAANSUhEUgAAAFAAAABQCAYAAACOEfKtAAAAfElEQVR42u3QMREAAAgEIE1u9LeD5wgR6EqmOGuBAgUKFIhAgQIFIlCgQIEIFChQIAIFChSIQIECBSJQoECBCBQoUCACBQoUiECBAgUiUKBAgQIFChQoUCACBQoUiECBAgUiUKBAgQgUKFAgAgUKFIhAgQIFIlCgQIEI/LNvE8dhTC3llgAAAABJRU5ErkJggg==";
+
 
 const ProductList = ({ products }) => {
+  const [openConformation, setOpenConformation] = useState(false);
+  const [currentProductId, setCurrentProductId] = useState(null);
+
+  const handleOpenConformation = (id) => {
+    setCurrentProductId(id);
+    setOpenConformation(true);
+  };
+
+  const handleCloseConformation= () => {
+    setOpenConformation(false);
+  };
+
   const handleDelete = async (id) => {
-    await deleteProduct(id);
-    await deleteLabel(id);
+    if (currentProductId) {
+      await deleteProduct(currentProductId);
+      await deleteLabel(currentProductId);
+    }
+    handleCloseConformation();
   };
 
   const handleEdit = (id) => {};
@@ -28,9 +49,21 @@ const ProductList = ({ products }) => {
       {products.map((product) => (
         <Card key={product.id}>
           <CardContent>
-            <Typography variant="h5" component="div">
-              {product.name}
-            </Typography>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+          <img
+            src={product.image ? `data:image/jpeg;base64,${product.image}` : `data:image/png;base64,${defaultImageBase64}`}
+            alt={product.name}
+            className="rounded-md object-cover"
+            style={{
+              height: "80px",
+              width: "80px",
+              marginRight: "20px" 
+            }}
+          />
+          <Typography variant="h5" component="div">
+            {product.name}
+          </Typography>
+          </div>
             <Typography color="text.secondary">
               Price: ${product.price}
             </Typography>
@@ -39,10 +72,19 @@ const ProductList = ({ products }) => {
             <Button
               size="small"
               color="primary"
-              onClick={() => handleDelete(product.productId)}
+              onClick={() => handleOpenConformation(product.productId)}
             >
               Delete
             </Button>
+            <ConfirmationDialog
+              open={openConformation}
+              onClose={handleCloseConformation}
+              onConfirm={handleDelete}
+              title="Confirm Deletion"
+            >
+              Are you sure you want to delete this product?
+            </ConfirmationDialog>
+
             <Button
               style={{ position: "absolute", top: 0, right: 0 }}
               size="small"
