32feecb85150c7ac6d737cc827e45d09ddeba645
b21946022 <b21946022@cs.hacettepe.edu.tr>
2024-04-25 22:19:48 +0300

    query taken from user done, search results update.

--- frontend/sombrero/src/pages/SearchResults.js
+++ frontend/sombrero/src/pages/SearchResults.js
@@ -1,34 +1,64 @@
 import React, { useState, useEffect } from 'react';
-import { Stack, Paper, Typography } from '@mui/material';
-import { searchProducts } from '../services/SearchService'; // Import the searchProducts function
+import { Stack, Card, CardContent, Typography } from '@mui/material';
+import { useLocation } from 'react-router-dom';
+import { searchProducts } from '../services/SearchService';
 import Header from "./Header";
 
 const SearchPage = () => {
   const [products, setProducts] = useState([]);
+  const location = useLocation(); // Get location object to access query parameter
 
   useEffect(() => {
-    // Fetch product data when component mounts
+    // Fetch product data when component mounts or when the query parameter changes
     const fetchData = async () => {
       try {
-        const response = await searchProducts("bed");
-        setProducts(response.data); // Set the fetched products in state
+        const query = new URLSearchParams(location.search).get('q'); // Get query parameter from URL
+        if (query) {
+          const response = await searchProducts(query);
+          setProducts(response.data); // Set the fetched products in state
+        }
       } catch (error) {
         console.error('Error fetching product data:', error);
       }
     };
 
     fetchData(); // Call the fetchData function
-  }, []);
+  }, [location.search]); // Update effect when query parameter changes
 
   return (
     <Header>
-      <Stack spacing={2}>
+      <Stack spacing={2} alignItems="center" sx={{ marginTop: '20px'}} >
         {products.map((product) => (
-          <Paper elevation={3} key={product.productId}>
-            <Typography variant="h5">{product.name}</Typography>
-            <Typography variant="body1">Price: ${product.price}</Typography>
-            {}
-          </Paper>
+          <Card elevation={3} key={product.productId} sx={{ width: 480, backgroundColor: '#f0f0f0' }}>
+            <CardContent>
+            <div style={{ display: "flex", alignItems: "center" }}>
+                {product.image ? (
+                  <img
+                    src={`data:image/jpeg;base64,${product.image}`}
+                    alt={product.name}
+                    style={{
+                      height: "120px",
+                      width: "120px",
+                      marginRight: "20px",
+                    }}
+                  />
+                ) : (
+                  <div style={{ 
+                    height: "120px", 
+                    width: "120px", 
+                    marginRight: "20px",
+                    backgroundColor: "#333" }}>
+                </div>
+                )}
+                <Typography variant="h5" component="div">
+                  {product.name}
+                </Typography>
+              </div>
+              <Typography color="text.secondary">
+                Price: ${product.price}
+              </Typography>
+            </CardContent>
+          </Card>
         ))}
       </Stack>
     </Header>
