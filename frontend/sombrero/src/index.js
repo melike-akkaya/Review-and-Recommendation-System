8e316c800e622c5d84cd10e35f6b2d1ae962f75a
Rauf Yanmaz <b2210356122@cs.hacettepe.edu.tr>
2024-05-12 19:07:43 +0300

    Login signup ui

--- frontend/sombrero/src/index.js
+++ frontend/sombrero/src/index.js
@@ -6,13 +6,20 @@ import App from "./pages/App";
 import MerchantProfile from "./pages/MerchantProfile";
 import SearchResults from "./pages/SearchResults";
 import ProductProfile from "./pages/ProductProfile";
+import LogIn  from "./pages/LogIn";
+import SignUp  from "./pages/SignUp";
+import SignUpasMerchant  from "./pages/SignUpasMerchant";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/merchant", element: <MerchantProfile /> },
   { path: "/search", element: <SearchResults /> },
   { path: "/product/:productId", element: <ProductProfile /> },
+  { path: "/login", element: <LogIn />},
+  { path: "/signup", element: <SignUp />},
+  { path: "/signupasmerchant", element: <SignUpasMerchant />}
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={router} />);
+
