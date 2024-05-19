0d79332b7801fcc4ca4b7311281822049760f2c7
melike akkaya <91731736+b2210356124@users.noreply.github.com>
2024-05-19 16:08:34 +0300

    analytics dialog

--- frontend/sombrero/src/components/merchant/AnalyticsDialog.js
+++ frontend/sombrero/src/components/merchant/AnalyticsDialog.js
@@ -1,96 +1,116 @@
-import { Dialog, DialogContent, DialogTitle } from '@mui/material';
-import { Button, Card, CardContent, Grid, Typography } from '@mui/material';
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import { getVisitCount } from '../../services/ProductService';
-import { getWishlistCount } from '../../services/WishlistService';
-import { useEffect, useState } from 'react';
-import React from 'react';
+import { Dialog, DialogContent, DialogTitle } from "@mui/material";
+import { Button, Grid } from "@mui/material";
+import { BarChart } from "@mui/x-charts/BarChart";
+import { getVisitCount } from "../../services/ProductService";
+import { getWishlistCount } from "../../services/WishlistService";
+import { useEffect, useState } from "react";
+import React from "react";
+import Stack from "@mui/material/Stack";
+import LinearProgress from "@mui/material/LinearProgress";
 
-const AnalyticsDialog = ({open,setOpen,products}) => {
-    const [wishlistCounts, setWishlistCounts] = useState({});
-    const [visitCounts, setVisitCounts] = useState({});
+const AnalyticsDialog = ({ open, setOpen, products }) => {
+  const [wishlistCounts, setWishlistCounts] = useState([]);
+  const [visitCounts, setVisitCounts] = useState([]);
+  const [chartDataReady, setChartDataReady] = useState(false);
 
-    useEffect(() => {
-        const fetchWishlistCounts = async () => {
-            const localWishListCounts = {};
-            const localVisitCounts = {};
-            await Promise.all(
-                products.map(async (product) => {
-                    if (product && product.productId) { // Check if product and product.id are defined
-                        try {
-                            const wishCount = await getWishlistCount(product.productId);
-                            const visitCount = await getVisitCount(product.productId);
-                            localWishListCounts[product.productId] = wishCount;
-                            localVisitCounts[product.productId] = visitCount;
-                        } catch (error) {
-                            localWishListCounts[product.productId] = 'N/A';
-                            localVisitCounts[product.productId] = 'N/A';
-                        }
-                    }
-                })
-            );
-            setWishlistCounts(localWishListCounts);
-            setVisitCounts(localVisitCounts);
+  useEffect(() => {
+    const fetchCounts = async () => {
+      const localWishListCounts = [];
+      const localVisitCounts = [];
 
-        };
+      await Promise.all(
+        products.map(async (product) => {
+          if (product && product.productId) {
+            try {
+              const wishCountResponse = await getWishlistCount(
+                product.productId
+              );
+              const visitCountResponse = await getVisitCount(product.productId);
 
-        if (products.length > 0) {
-            fetchWishlistCounts();
-        }
-    }, [products]);
+              localWishListCounts.push(wishCountResponse);
+              if (visitCountResponse.data == "") {
+                localVisitCounts.push(0);
+              } else {
+                localVisitCounts.push(visitCountResponse.data);
+              }
+            } catch (error) {
+              console.error(
+                `Error getting counts for product ${product.productId}:`,
+                error
+              );
+              // assuming 0 for simplicity:
+              localWishListCounts.push(0);
+              localVisitCounts.push(0);
+            }
+          }
+        })
+      );
 
-    const handleClose = () => {
-        setOpen(false);
+      setWishlistCounts(localWishListCounts);
+      setVisitCounts(localVisitCounts);
+      setChartDataReady(true);
     };
 
+    if (products.length > 0) {
+      fetchCounts();
+    }
+  }, [products]);
 
-    return (
-        <div>
-            <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
-                <DialogTitle>Analytics</DialogTitle>
-                <DialogContent dividers style={{ maxHeight: '70vh', overflowY: 'auto' }}>
-                    {products.map((product) => (
-                    <Card key={product.productId} style={{ marginBottom: '16px' }}>
-                    <CardContent>
-                        <div style={{ display: "flex", alignItems: "center" }}>
-                        {product.image ? (
-                            <img
-                            src={`data:image/jpeg;base64,${product.image}`}
-                            alt={product.name}
-                            className="rounded-md object-cover"
-                            style={{
-                                height: "80px",
-                                width: "80px",
-                                marginRight: "20px",
-                            }}
-                            />
-                        ) : (
-                            <ShoppingBasketIcon />
-                        )}
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-                        <Typography variant="h5" component="div">
-                            {product.name}
-                        </Typography>
-                        </div>
-                        <Typography color="text.secondary">
-                        Price: ${product.price}
-                        </Typography>
-                        <Typography color="text.secondary">
-                        WishListCount: {wishlistCounts[product.productId]}
-                        </Typography>
-                        <Typography color="text.secondary">
-                        VisitCount: {visitCounts[product.productId]}
-                        </Typography>
-                    </CardContent>
-                    </Card>
-                    ))}
-                    <Grid item xs={12} align="right">
-                    <Button onClick={handleClose}>Cancel</Button>
-                    </Grid>
-                </DialogContent>
-            </Dialog>
-        </div>
-    );
+  return (
+    <div>
+      <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
+        <DialogTitle>Analytics</DialogTitle>
+        <DialogContent
+          dividers
+          style={{ maxHeight: "70vh", overflowY: "auto" }}
+        >
+          {chartDataReady ? (
+            <BarChart
+              series={[
+                {
+                  data: wishlistCounts,
+                  label: "Wishlist Counts",
+                  color: "#ff9ff3",
+                },
+                {
+                  data: visitCounts,
+                  label: "Visit Counts",
+                  color: "#48dbfb",
+                },
+              ]}
+              height={400}
+              width={600}
+              layout="horizontal"
+              xAxis={[
+                {
+                  scaleType: "linear",
+                },
+              ]}
+              yAxis={[
+                {
+                  data: products.map((product) => product.name),
+                  scaleType: "band",
+                  tickSize: 10,
+                },
+              ]}
+              margin={{ left: 150 }}
+            />
+          ) : (
+            <Stack sx={{ width: "100%", color: "grey.500" }} spacing={2}>
+              <LinearProgress color="inherit" />
+            </Stack>
+          )}
+          <Grid item xs={12} align="right" style={{ marginTop: "16px" }}>
+            <Button onClick={handleClose}>Cancel</Button>
+          </Grid>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 };
 
 export default AnalyticsDialog;
